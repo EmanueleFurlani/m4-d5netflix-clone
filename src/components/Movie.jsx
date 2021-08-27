976d4c76fc5867ff69d061ca6217e83edcf751e5
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-08-27 14:31:46 +0200

    added anhor to each poster

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import { Component } from 'react'
+import React from "react";
+import { Component } from "react";
 // import CommentsDropdown from './CommentsDropdown'
 
-
-
 class Movie extends Component {
-	state = {
-		Poster: this.props.movie.Poster,
-		Title: this.props.movie.Title,
-		Type: this.props.movie.Type,
-		Year: this.props.movie.Year,
-		imdbID: this.props.imdbID
-	}
-	
-	render() {
-		return (
-			<div className="col-md-2 col-lg-2 movie-container mx-0 px-1">
-				<img src={this.state.Poster} className="img-fluid carousel-img" alt="Movie Here"/>
-				{/* <CommentsDropdown /> */}
-			</div>
-		)
-	}
+  state = {
+    Poster: this.props.movie.Poster,
+    Title: this.props.movie.Title,
+    Type: this.props.movie.Type,
+    Year: this.props.movie.Year,
+    imdbID: this.props.imdbID,
+  };
+
+  render() {
+    return (
+      <div className="col-md-2 col-lg-2 movie-container mx-0 px-1">
+        <a onClick={() => console.log(this.state)}>
+          <img
+            src={this.state.Poster}
+            className="img-fluid carousel-img"
+            alt="Movie Here"
+          />
+        </a>
+        {/* <CommentsDropdown /> */}
+      </div>
+    );
+  }
 }
 
-export default Movie
+export default Movie;
