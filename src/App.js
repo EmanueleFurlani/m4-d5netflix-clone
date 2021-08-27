87a9b85aaab91a9e005b192b1d9a019044d72ec6
emanuele <emanuelefurlani.go@gmail.com>
2021-08-27 14:16:57 +0200

    upload nav+head+footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import NetflixNavbar from "./components/NetflixNavbar"
+import Header from './components/Header'
+import NetflixFooter from "./components/NetflixFooter"
+import SearchBar from './components/SearchBar';
+import fantasyBooks from './fantasyBooks.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NetflixNavbar />
+      <Header />
+      <SearchBar  books={fantasyBooks}/>
+      <NetflixFooter />
+    </>
   );
 }
 
